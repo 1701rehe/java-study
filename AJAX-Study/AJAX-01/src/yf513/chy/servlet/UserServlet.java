200a4743faf8abf9b675fd9d54c71f2824735e96
chy <251134852@qq.com>
2020-11-19 10:13:01 +0800

    ajax-01

--- AJAX-Study/AJAX-01/src/yf513/chy/servlet/UserServlet.java
+++ AJAX-Study/AJAX-01/src/yf513/chy/servlet/UserServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-@WebServlet("/userServlet")
+@WebServlet("/userServlet1")
 public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
